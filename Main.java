b20589e1fcbdd42c3c04d6068a78184a82e12cdd
datboisus <donvu251106@gmail.com>
2024-10-29 10:17:02 -0700

    1

--- Main.java
+++ Main.java
@@ -5,12 +5,44 @@ public class Main
   public static void main(String[] args)
   {
     // TODO: Problem 1
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter two numbers:");
+    int x = sc.nextInt();
+    int y = sc.nextInt();
 
-
+    if (y == 0)
+    {
+      System.out.println("...");
+  
+    }
+    else
+    {
+      double ratio = (double) x / y;
+        if (1 < ratio && ratio <= 8)
+        {
+          System.out.println("Ratio Ok");
+        }
+    }
     // TODO: Problem 2
+    System.out.println("enter two numbers:");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
 
-
+    if (b == 0)
+    {
+      System.out.println("error");
+    }
+    else
+    {
+      if (a % b == 0)
+      {
+        System.out.println("it is a factor");
+      }
+    }
     // TODO: Problem 3
+    
+  
+  
   }
 
   // TODO: In-class assignment
